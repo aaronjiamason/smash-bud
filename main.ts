076faaee3386eec08ce20eacef34e5151a18b769
aaronjiamason <122560100+aaronjiamason@users.noreply.github.com>
2023-03-01 13:29:15 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,19 @@
-tiles.setCurrentTilemap(tilemap`platform`)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (kirbojumpcount <= 1) {
+        kirbs.vy = -120
+        kirbojumpcount += 1
+    }
+})
+let kirbojumpcount = 0
+let kirbs: Sprite = null
+tiles.setCurrentTilemap(tilemap`level2`)
 scene.setBackgroundColor(6)
-let kirbs = sprites.create(assets.image`kirbo`, SpriteKind.Player)
-controller.moveSprite(kirbs)
+kirbs = sprites.create(assets.image`kirbo`, SpriteKind.Player)
+controller.moveSprite(kirbs, 100, 0)
+kirbs.ay = 200
+kirbojumpcount = 2
+game.onUpdate(function () {
+    if (kirbs.isHittingTile(CollisionDirection.Bottom)) {
+        kirbojumpcount = 0
+    }
+})
